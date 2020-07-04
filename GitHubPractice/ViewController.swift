5ac1c654ecf2f309dccedd12da89f313be30415a
satish reddy <31077192+satish1219@users.noreply.github.com>
2020-07-04 10:59:10 +0530

    Initial Commit

---
+++ GitHubPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubPractice
+//
+//  Created by Satish Reddy on 04/07/20.
+//  Copyright © 2020 Satish Reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
